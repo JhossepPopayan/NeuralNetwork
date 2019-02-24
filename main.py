1fe28857c2af932c267cef765a11f031a3df9daf
Chophry <jhossep.popayan@pucp.pe>
2019-02-24 21:08:25 +0100

    Update Code

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 from classes.neural import NeuralNetwork
-#import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-ne = 2
-nm = 5
+start = -2
+end = 3
+step = 0.05
+x = np.arange(start,end+step,step) # Vector from 0 - 1 
+N = x.shape[0]
+a, b, c = 1, 2, 3
+noise = np.random.normal(0.2,0.4,(N,))
+y = a*np.power(x,2) + b*x + c + 0*noise
+
+ne = 1
+nm = 3
+num_out = 1
 layers = 1 
 bias = True
 act_fun = "sigmoid_1"
-x = NeuralNetwork(ne, layers, nm, bias, act_fun)
 
-#print("Weights1: ", x.weight_u())
-#print("Weights2: ", x.weight_w())
-print(x.execute())
+nn = NeuralNetwork(ne, x, num_out, y, layers, nm, bias, act_fun)
+
+learn_rate = 0.1
+max_iter = 10000
+
+yd, J, Iter =nn.execute(learn_rate,max_iter)
+
+plt.figure(1)
+plt.plot(x,y,'*',x,yd,'-r')
+plt.figure(2)
+plt.plot(np.arange(Iter),J)
+plt.show()
