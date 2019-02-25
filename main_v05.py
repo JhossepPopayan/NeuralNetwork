6b53123b31b01c10429500583f1d363dcad00beb
Joel-Tafur <41816872+Joel-Tafur@users.noreply.github.com>
2019-02-25 16:48:05 -0500

    Todo corre
    revisar valores de los pesos actualizados

---
+++ main_v05.py
@@ -0,0 +1,27 @@
+from classes.neural_v05 import NeuralNetwork
+import matplotlib.pyplot as plt
+import numpy as np
+
+start = -2
+end = 3
+step = 0.05
+x=np.zeros((101,2))
+x[:,0] = np.arange(start,end+step,step) # Vector from 0 - 1 
+x[:,1] = np.arange(start,end+step,step) # Vector from 0 - 1                                                                 
+N = x.shape[0]
+a, b, c = 1, 2, 3
+noise = np.random.normal(0.2,0.4,(N,))
+y = a*np.power(x[:,0],2) + b*x[:,1] + c + 0*noise
+ne = 2
+nm = 3
+num_out = 2
+layers = 1
+bias = True
+act_fun = "sigmoid_1"
+
+nn = NeuralNetwork(ne, x, num_out, y, layers, nm, bias, act_fun)
+
+learn_rate = 1e-3
+max_iter = 1000000
+
+nn.execute(learn_rate,max_iter)
