1b8b57c3a71897ac72c03be900f019a4449a67e8
Joel-Tafur <41816872+Joel-Tafur@users.noreply.github.com>
2019-02-27 15:16:32 -0500

    Update and rename neural_v05.py to neural.py

--- classes/neural.py
+++ classes/neural.py
@@ -123,31 +123,3 @@ class NeuralNetwork:
             w1 += -rate*dL_dw1
         y_pred = self.inference(X, [w1, w2])
         return y_pred
-'''
-if __name__ == '__main__':
-    start = -2
-    end = 3
-    step = 0.05
-    y=np.zeros((101,1))
-    x=np.zeros((101,2))
-    x[:,0] = np.arange(start,end+step,step) # Vector from 0 - 1 
-    x[:,1] = np.arange(start,end+step,step) # Vector from 0 - 1                                                                 
-    N = x.shape[0]
-    a, b, c = 1, 2, 3
-    noise = np.random.normal(0.2,0.4,(N,))
-    y = np.array([a*np.power(x[:,0],2) + b*x[:,1] + c + 0*noise]).T
-    ne = 2
-    nm = 3
-    num_out = 1
-    layers = 1
-    bias = True
-    act_fun = "sigmoid_1"
-
-    nn = NeuralNetwork(ne, x, num_out, y, layers, nm, bias, act_fun)
-
-    learn_rate = 0.1
-    max_iter = 10000
-
-    yd=nn.execute(learn_rate,max_iter)
-    print(yd)
-    '''
